f9161c1372782cf0dcbd5d7e663e37283875ccae
Ethan <ethanjosephbrown12@gmail.com>
2020-08-17 15:24:04 -0500

    Fixed MANY THINGS(Animations actually work now)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
     public Animator anim;
+    public Rigidbody2D rb;
+
+    public int jumpForce;
+    public bool grounded;
+    public Transform groundPoint;
+    public LayerMask groundLayer;
     // Start is called before the first frame update
+
+    private void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapPoint(groundPoint.position,groundLayer);
+    }
     void Start(){
      
     }
 
     // Update is called once per frame
     void Update(){
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (grounded && Input.GetKeyDown(KeyCode.Space)){
             anim.SetTrigger("Jump");
+            rb.AddForce(Vector2.up * jumpForce);
         }
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("Grounded", grounded);
     }
 }
